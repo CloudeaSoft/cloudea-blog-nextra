10d4b8175be5bbba0ab5c73a273ab13da2b09aa1
Cloudea <cloudeasoft@qq.com>
2025-09-09 21:38:44 +0800

    feat: add hitokoto support in home page

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { GetHitokoto } from "../utils/hitokoto";
+
+export default async function TagPage(props) {
+  const hito = await GetHitokoto();
+  return (
+    <>
+      <h1>Archives</h1>
+      {hito != null && <div>{hito.hitokoto} —— {hito.from_who ?? hito.from}</div>}
+    </>
+  );
+}
---
+++ utils/hitokoto.ts
@@ -0,0 +1,17 @@
+const HITOKOTO_URL: string = "https://v1.hitokoto.cn";
+
+export const GetHitokoto = async (): Promise<Hitokoto> => {
+  try {
+    const response = await fetch(HITOKOTO_URL);
+    const data = await response.json();
+
+    if (data && typeof data === "object" && (data as Hitokoto).hitokoto) {
+      return data;
+    }
+
+    throw new Error("Hitokoto is inavailable for unknown reason.");
+  } catch (ex) {
+    console.warn(ex.message);
+    return null;
+  }
+};
